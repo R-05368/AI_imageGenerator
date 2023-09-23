8f95433d415a6ebfff975e7d344ebded2ecbc03c
R-05368 <73303315+R-05368@users.noreply.github.com>
2023-09-23 17:45:21 +0530

    First Commit
    Date: 20.09.23

---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
+
+const router = express.Router();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+
+router.route('/').post(async (req, res) => {
+  try {
+    const response = await openai.createImage({
+      prompt: "a white siamese cat",
+      n: 1,
+      size: "1024x1024",
+    });
+    
+
+    const image = response.data.data[0].url;
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+  }
+});
+
+export default router;
